f66afa8440362c6c0b02ac47cf742097fe16aeab
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2021-05-18 22:24:56 -0300

    Filtre la busqueda segun cada campo

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 style="text-align: center;">Buscador Autos</h1>
+
+---
+
+## JavaScript Moderno Project #3
+
+### Overview
+
+![Preview](https://i.ibb.co/1dTsJ7D/Buscador-Autos.png)
+
+- Aprendi a filtrar la busqueda comprobando cada campo
+
+[Udemy Course](https://www.udemy.com/course/javascript-moderno-guia-definitiva-construye-10-proyectos/)
+
+### Contact
+
+- Website [jruizsilva.netlify.app](https://jruizsilva.netlify.app)
+- GitHub [@jruizsilva](https://github.com/jruizsilva)
+- Twitter [@jnt077](https://twitter.com/jnt077)
--- js/script.js
+++ js/script.js
@@ -37,27 +37,124 @@ const llenarYear = () => {
 	const minYear = 2015;
 	for (let i = minYear; i <= currentYear; i++) {
 		const option = document.createElement("option");
+		option.value = i;
 		option.textContent = i;
 
 		selectYear.appendChild(option);
 	}
 };
-const filtrar = (e) => {
-	// Funciones para filtrar
-	console.log(e.target.value);
-	const porMarca = (auto) =>
-		auto.marca.toLowerCase() === e.target.value.toLowerCase();
-	const result = autos.filter(porMarca);
+const filtrar = () => {
+	const porMarca = (auto) => {
+		if (busqueda.marca) {
+			return busqueda.marca.toUpperCase() === auto.marca.toUpperCase();
+		} else {
+			return auto;
+		}
+	};
+	const porYear = (auto) => {
+		if (busqueda.year) {
+			return Number(busqueda.year) === auto.year;
+		} else {
+			return auto;
+		}
+	};
+	const porPrecioMinimo = (auto) => {
+		if (busqueda.min) {
+			return Number(busqueda.min) < auto.precio;
+		} else {
+			return auto;
+		}
+	};
+	const porPrecioMaximo = (auto) => {
+		if (busqueda.max) {
+			return Number(busqueda.max) > auto.precio;
+		} else {
+			return auto;
+		}
+	};
+	const porCantidadPuertas = (auto) => {
+		if (busqueda.puertas) {
+			return Number(busqueda.puertas) === auto.puertas;
+		} else {
+			return autos;
+		}
+	};
+	const porTransmision = (auto) => {
+		if (busqueda.transmision) {
+			return (
+				busqueda.transmision.toLowerCase() === auto.transmision.toLowerCase()
+			);
+		} else {
+			return auto;
+		}
+	};
+	const porColor = (auto) => {
+		if (busqueda.color) {
+			return busqueda.color.toLowerCase() === auto.color.toLowerCase();
+		} else {
+			return auto;
+		}
+	};
+	const result = autos
+		.filter(porMarca)
+		.filter(porYear)
+		.filter(porPrecioMinimo)
+		.filter(porPrecioMaximo)
+		.filter(porCantidadPuertas)
+		.filter(porTransmision)
+		.filter(porColor);
 	mostrarAutos(result);
 };
 // Variables
 const marca = document.querySelector("#marca");
-const filtrado = [];
+const year = document.querySelector("#year");
+const min = document.querySelector("#precio-min");
+const max = document.querySelector("#precio-max");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+const busqueda = {
+	marca: "",
+	year: "",
+	min: "",
+	max: "",
+	puertas: "",
+	color: "",
+	transmision: "",
+};
 
 // Eventos
 const cargarEventos = () => {
 	document.addEventListener("DOMContentLoaded", iniciarApp);
-	marca.addEventListener("change", filtrar);
+	marca.addEventListener("change", (e) => {
+		busqueda.marca = e.target.value;
+		filtrar();
+	});
+	year.addEventListener("change", (e) => {
+		busqueda.year = e.target.value;
+		filtrar();
+	});
+	min.addEventListener("change", (e) => {
+		busqueda.min = e.target.value;
+		filtrar();
+	});
+	max.addEventListener("change", (e) => {
+		busqueda.max = e.target.value;
+		filtrar();
+	});
+	puertas.addEventListener("change", (e) => {
+		busqueda.puertas = e.target.value;
+		filtrar();
+	});
+	color.addEventListener("change", (e) => {
+		busqueda.color = e.target.value;
+		filtrar();
+	});
+	transmision.addEventListener("change", (e) => {
+		busqueda.transmision = e.target.value;
+		filtrar();
+	});
 };
 
 cargarEventos();
